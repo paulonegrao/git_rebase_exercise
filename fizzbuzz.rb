f0f4ba960a403513227f0ffe79bb767a4d6c80c7
Paulo Negrao <paulo.negrao@ymail.com>
2015-11-09 15:10:43 -0800

    teste

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ first_word = gets.chomp
 
 print "Give me a word (e.g. BUZZ): "
 second_word = gets.chomp
-
+puts "hkjhjhhkhkhjk"
 for x in 1..100
   if x % 3 == 0 && x % 5 == 0
     puts "#{first_word}#{second_word}"
